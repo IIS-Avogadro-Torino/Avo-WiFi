695a4e4cfa882c261219f7af3c16c880cf5a49e5
MarcoSchiavello <marco.schiavello4@gmail.com>
2022-03-24 20:32:44 +0100

    added pages for auth and confirm tokens

---
+++ src/authCode.php
@@ -0,0 +1,42 @@
+<?php
+######################################################################
+# Wi-Fi-voucher-o-matic-2.0
+# Copyright (C) 2022 Marco Schiavello, Ivan Bertotto, ITIS Avogadro
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.If not, see <http://www.gnu.org/licenses/>.
+######################################################################
+
+include_once 'load.php';
+include_once LIB_PATH.'/library.php';
+
+printHead('Richiesta Token', 
+          [ 'style.css' ],
+          [ ],
+          false);
+?>
+
+<?php include_once COMP_PATH.'/logoBox.php';?>
+
+<div class="page-cont container container--gapM page-size">
+    <h1 class="page-cont__title">Immetti codice conferma</h1>
+    
+    <form class="page-cont__form container container--gapS" method="POST" action="authCode.php">
+        <?php 
+            inputText("authCode", "Codice autenticazione"); 
+        ?>
+    </form>
+
+    <input class="button" type="submit" name="authCodeSubmit" value="Verifica">
+</div>
+
+<?php printFooter(); ?>
---
+++ src/confirmToken.php
@@ -0,0 +1,40 @@
+<?php
+######################################################################
+# Wi-Fi-voucher-o-matic-2.0
+# Copyright (C) 2022 Marco Schiavello, Ivan Bertotto, ITIS Avogadro
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.If not, see <http://www.gnu.org/licenses/>.
+######################################################################
+
+include_once 'load.php';
+include_once LIB_PATH.'/library.php';
+
+printHead('Richiesta Token', 
+          [ 'style.css' ],
+          [ ],
+          false);
+?>
+
+<?php include_once COMP_PATH.'/logoBox.php';?>
+
+<div class="page-cont container container--gapM page-size">
+    <h1 class="page-cont__title">Token inviato con successo</h1>
+    
+    <p>
+        Ciao Nome Cognome, il tuo token per accedere al Wi-fi 
+        è stato mandato alla seguente E-Mail:
+    </p>
+    <p>E-mail</p>
+</div>
+
+<?php printFooter(); ?>
